7b53087addfa456a7044785f563a5b860578b426
blacha370 <lukasz.blacha370@gamil.com>
2022-04-17 12:37:17 +0200

    Update get-config.sh

--- get-config.sh
+++ get-config.sh
@@ -1,4 +1,15 @@
 #! /bin/bash
 
-cp ~/.xinitrc .
-cp -r ~/.config .
+configDirs=("alacritty" "i3" "i3status" "nitrogen" "ulauncher")
+configFiles=(".xinitrc")
+
+mkdir -p .config-files
+
+for file in ${configFiles[@]}; do
+	cp ~/$file .config-files/$file
+done
+
+for dir in ${configDirs[@]}; do
+	mkdir -p .config/$dir
+	cp -r ~/.config/$dir .config/
+done
