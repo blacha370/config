a5790da9df5f59284e6b388cc3a36b2184d179ce
blacha370 <lukasz.blacha370@gamil.com>
2022-04-17 13:11:36 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
 
 pacmanPackages=$(comm -12 <(pacman -Slq | sort) <(sort pacman.txt))
-pacman -S --needed $pacmanPackages
+sudo pacman -S --needed $pacmanPackages
+
+IFS=$'\n' read -d '' -r -a yayPackages < yay.txt
+
+for package in "${yayPackages[@]}"; do
+	yay -S $package
+done
+
+cp -r .config ~/.config
+cp -r .config-files/. ~/
